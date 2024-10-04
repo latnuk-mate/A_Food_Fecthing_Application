deeeef7533fe5718eadfcadcb8c6d44c914f10c4
latnuk <kuntalhome999@gmail.com>
2024-10-05 01:51:37 +0530

    create another route

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req,res)=>{
     res.end("hello world!");
 });
 
+app.get('/dashboard', (req,res)=>{
+    res.end("Welcome to the dashboard!");
+});
+
 
 app.listen(PORT, ()=>{
     console.log('server is running on port ', PORT);
